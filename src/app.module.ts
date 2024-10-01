65c0efd3ca990bda9b0846811715938cfac6375f
Abhik Patra <theabhik2020@gmail.com>
2024-10-01 09:12:32 +0530

    mongo_and_env_setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      ignoreEnvFile: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URl),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
